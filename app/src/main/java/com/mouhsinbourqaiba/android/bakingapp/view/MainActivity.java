b3a50e0b75d413cc2532b684fdcdbf141e60f5e5
mouhsinandroid <mouhsinbourqaiba@gmail.com>
2020-04-23 14:41:54 +0200

    chore: first comment

---
+++ app/src/main/java/com/mouhsinbourqaiba/android/bakingapp/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mouhsinbourqaiba.android.bakingapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mouhsinbourqaiba.android.bakingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
